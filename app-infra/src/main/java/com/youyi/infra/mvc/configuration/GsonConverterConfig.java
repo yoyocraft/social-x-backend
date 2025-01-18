1936e7994e28d7a0abe2d06eb443bb94a528a2f9
youyi <youyichannel@163.com>
2025-01-18 11:48:43 +0800

    feat(openapi): add openapi document

--- app-infra/src/main/java/com/youyi/infra/mvc/configuration/GsonConverterConfig.java
+++ app-infra/src/main/java/com/youyi/infra/mvc/configuration/GsonConverterConfig.java
@@ -2,10 +2,16 @@ package com.youyi.infra.mvc.configuration;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import java.lang.reflect.Type;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import springfox.documentation.spring.web.json.Json;
 
 /**
  * @author <a href="https://github.com/yoyocraft">yoyocraft</a>
@@ -14,11 +20,21 @@ import org.springframework.http.converter.json.GsonHttpMessageConverter;
 @Configuration
 public class GsonConverterConfig {
 
+    private final Gson gson = new GsonBuilder()
+        .registerTypeAdapter(Json.class, new SpringfoxJsonToGsonAdapter())
+        .create();
+
     @Bean(name = "gsonHttpMessageConverters")
     public HttpMessageConverters gsonHttpMessageConverters() {
         GsonHttpMessageConverter gsonConverter = new GsonHttpMessageConverter();
-        Gson gson = new GsonBuilder().create();
         gsonConverter.setGson(gson);
         return new HttpMessageConverters(gsonConverter);
+    }
+
+    private static class SpringfoxJsonToGsonAdapter implements JsonSerializer<Json> {
+        @Override
+        public JsonElement serialize(Json json, Type type, JsonSerializationContext context) {
+            return JsonParser.parseString(json.value());
+        }
     }
 }
